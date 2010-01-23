33697d28b73adc5e50e77cf65bbfc3c0109b7309
mbrakels <mbrakels@482fb736-89cc-11de-832f-d15e35d84da2>
2010-01-23 14:43:30 +0000

    Bug fixes(part 1/3)

---
+++ trunk/legomindstorm/C api2/C api/Lego MindStorm Control Api/Lego MindStorm Control Api/Form2.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Lego_MindStorm_Control_Api
+{
+    partial class Fconfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCancel = new System.Windows.Forms.Button();
+            this.btOk = new System.Windows.Forms.Button();
+            this.tbConfig = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(416, 388);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(85, 24);
+            this.btCancel.TabIndex = 0;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // btOk
+            // 
+            this.btOk.Location = new System.Drawing.Point(324, 388);
+            this.btOk.Name = "btOk";
+            this.btOk.Size = new System.Drawing.Size(77, 24);
+            this.btOk.TabIndex = 1;
+            this.btOk.Text = "Ok";
+            this.btOk.UseVisualStyleBackColor = true;
+            this.btOk.Click += new System.EventHandler(this.btOk_Click);
+            // 
+            // tbConfig
+            // 
+            this.tbConfig.Location = new System.Drawing.Point(12, 12);
+            this.tbConfig.Multiline = true;
+            this.tbConfig.Name = "tbConfig";
+            this.tbConfig.Size = new System.Drawing.Size(488, 370);
+            this.tbConfig.TabIndex = 2;
+            // 
+            // Fconfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 424);
+            this.Controls.Add(this.tbConfig);
+            this.Controls.Add(this.btOk);
+            this.Controls.Add(this.btCancel);
+            this.Name = "Fconfig";
+            this.Text = "config editor";
+            this.Deactivate += new System.EventHandler(this.btCancel_Click);
+            this.Load += new System.EventHandler(this.Fconfig_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Button btOk;
+        private System.Windows.Forms.TextBox tbConfig;
+    }
+}
