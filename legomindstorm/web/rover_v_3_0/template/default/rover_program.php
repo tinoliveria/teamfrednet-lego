82a204dac2b3044d8ab1692f00fcea9a699b93bc
mbrakels <mbrakels@482fb736-89cc-11de-832f-d15e35d84da2>
2010-01-31 17:02:22 +0000

    New Version is comming soon(demo on next meeting) Changes: * logout add * no leaving when claiming time * Add web part for NBC/NXC code * some bug fixes

--- legomindstorm/web/rover_v_3_0/template/default/rover_program.php
+++ legomindstorm/web/rover_v_3_0/template/default/rover_program.php
@@ -1,1 +1,11 @@
-Work in progress
+<input type="button" onclick="new_rover_program();"
+	value="Create new rover Program" />
+<br />
+Enter ID:
+<input id="rover_program_id" />
+<input
+	type="button" value="Load" onclick="rover_program_id();" />
+<br />
+<div id="rover_program"></div>
+<iframe id="hiddenframe" name="hiddenframe"
+	src="about:blank" height="100" width="500" frameborder="0"></iframe>
